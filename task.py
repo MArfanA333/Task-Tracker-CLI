2a5a6cae5e3a52b54c0feceaee2618e5835c96ba
Mohammad Arfan Ameen <marfana333@gmail.com>
2025-06-20 19:49:46 +0400

    Update Task.py
    Created Status(enum) class, and Task class.
    Added update_status(), string(), to_dict(), initialization to class.

--- task.py
+++ task.py
@@ -1,5 +1,51 @@
+import datetime
+import enum
+
+class Status(enum):
+    PENDING = "Pending"
+    IN_PROGRESS = "In Progress"
+    DONE = "Done"
+
 class Task:
-    def __init__(self):
+    SEPARATOR = "-----------------------------------------\n"
+    
+    def __init__(self, task_id, description):
         
+        self.id = task_id
+        self.description = description
+        self.status = Status.PENDING
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+    
+    def update_status(self, status):
         
-        return
+        self.status = status
+        self.updated_at = datetime.datetime.now()
+    
+    def __str__(self):
+        
+        string = self.SEPARATOR
+        string += f"Task #{self.id}:\n"
+        string += f"Description:\n{self.description}\n"
+        string += f"Current Status: {self.status.value}\n"
+        string += f"Created At: {self.created_at}\n"
+        string += f"Updated At: {self.updated_at}\n"
+        
+        return string
+    
+    def to_dict(self):
+        return {"id": self.id, "description": self.description, 
+                "status": self.status.value, "created_at": self.created_at.isoformat(),
+                "updated_at": self.updated_at.isoformat()}
+
+    @classmethod
+    def from_dict(cls, data:dict):
+        task_id = data["id"]
+        description = data["description"]
+        status = Status(data["status"])
+        
+        task = cls(task_id,description,status)
+        task.created_at = datetime.datetime.fromisoformat(data["created_at"])
+        task.updated_at = data["updated_at"]
+        
+        return task
